5a88a3cca32d5ef16343af4788ef7d61c1609bf6
Janis <janis.meister87@gmail.com>
2022-06-06 20:11:12 +0200

    productionobjects

---
+++ Assets/Scripts/ProductionObjects/IOPort.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IOPort : MonoBehaviour
+{
+    public bool isInput;
+    public ItemSO item;
+    public int itemCount = 0;
+    private int maxItemCount = 100;
+
+    public void IncreaseItemCount(int value)
+    {
+
+    }
+
+    public void DecreaseItemCount(int value)
+    {
+
+    }
+
+
+}
