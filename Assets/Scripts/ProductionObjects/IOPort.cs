44461ff9557eee11e060e5feacf6ba6fb25f07a7
Janis <janis.meister87@gmail.com>
2022-06-07 19:10:55 +0200

    io

--- Assets/Scripts/ProductionObjects/IOPort.cs
+++ Assets/Scripts/ProductionObjects/IOPort.cs
@@ -8,15 +8,35 @@ public class IOPort : MonoBehaviour
     public int itemCount = 0;
     private int maxItemCount = 100;
 
-    public void IncreaseItemCount(int value)
+    public int IncreaseItemCount(int value) // Returns rest of value
     {
+        int restOfValue = 0;
+        if (itemCount + value > maxItemCount)
+        {
+            restOfValue = value - (maxItemCount - itemCount);
+            itemCount = maxItemCount;
+        }
+        else
+        {
+            itemCount += value;
+        }
 
+        return restOfValue;
     }
 
-    public void DecreaseItemCount(int value)
+    public int DecreaseItemCount(int value) // Returns rest of value
     {
+        int restOfValue = 0;
+        if (itemCount - value < 0)
+        {
+            restOfValue = value - itemCount;
+            itemCount = 0;
+        }
+        else
+        {
+            itemCount -= value;
+        }
 
+        return restOfValue;
     }
-
-
 }
